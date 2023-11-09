add4408f7af2e293fb29bd5f4883e9008687444e
khushboo <khushboo20@navgurukul.org>
2023-11-09 22:39:17 +0530

    Created Signin page add event handler and state

--- src/Signin.js
+++ src/Signin.js
@@ -1,14 +1,29 @@
+import { useState } from "react";
+
 function Signin(){
+
+    const [email,setEmail]=useState("")
+    const [password,setPassword]=useState("")
+
+    const HandleEmail=(e)=>{
+        setEmail(e.target.value)
+    }
+
+    const HandlePassword=(e)=>{
+        setPassword(e.target.value)
+    }
+
+
+
     return(
         <>
-          <label>Name</label>
+          <label onChange={HandleEmail} value={email}>Name</label>
           <input placeholder="Name"></input><br></br>
 
           <label>password</label>
-          <input></input><br></br>
+          <input onChange={HandlePassword} placeholder="password" value={password}></input><br></br>
 
           <button>signin</button>
-
 
         </>
     )
